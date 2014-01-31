208e419783f9b401f0ae19e160dc2f584c569f8e
Trey Dockendorf <treydock@gmail.com>
2014-01-31 17:41:13 -0600

    Fix loading issue during Travis-CI tests for Puppet 3.0.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+ver = Gem::Version.new(Puppet.version.split('-').first)
+if Gem::Requirement.new("~> 3.0.0") =~ ver
+  puts "ovirt: setting LOAD_PATH to work around broken type autoloading"
+  fixtures_dir = File.join(File.dirname(__FILE__),  'fixtures')
+  $LOAD_PATH.unshift(File.join(fixtures_dir, 'modules/stdlib/lib'))
+end
+
 shared_context :defaults do
   let(:node) { 'foo.example.com' }
 
